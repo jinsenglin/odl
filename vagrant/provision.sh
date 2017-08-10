160c3080ffb522ce55ef7290624962a2bb012c8f
Jim Lin <jimlintw922@gmail.com>
2017-08-10 15:06:28 +0800

    update provision.sh

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-date | tee /tmp/date
-whoami | tee -a /tmp/date
+set -e
+
+LOG=/tmp/provision.log
+date | tee $LOG            # when
+whoami | tee -a $LOG       # who
+pwd | tee -a $LOG          # where
+
+function install_jdk() {
+    JDK_VERSION=8
+    apt-get update
+    apt-get install openjdk-$JDK_VERSION-jdk
+}
+
+function download_odl() {
+    ODL_VERSION=0.5.3-Boron-SR3
+    wget https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/integration/distribution-karaf/$ODL_VERSION/distribution-karaf-$ODL_VERSION.tar.gz
+}
+
+function main() {
+    :
+    install_jdk
+    #download_odl
+}
+main
