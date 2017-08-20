58fde5e682619f114c76cfca87a00e3bcbf4af55
Jim Lin <jimlintw922@gmail.com>
2017-08-20 21:57:37 +0800

    update verify-openstack.sh

--- with-openstack/verify-openstack.sh
+++ with-openstack/verify-openstack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Create the external network
 # Create the self-service network
 # Create a router
 # Create m1.nano flavor
@@ -8,6 +9,7 @@
 # Launch an instance
 # Access the instance remotely
 
+# Reference https://kairen.gitbooks.io/openstack-ubuntu-newton/content/ubuntu-binary/neutron/create-network.html
 # Reference https://docs.openstack.org/newton/install-guide-ubuntu/launch-instance-networks-selfservice.html
 # Reference https://docs.openstack.org/newton/install-guide-ubuntu/launch-instance.html#launch-instance
 # Reference https://docs.openstack.org/newton/install-guide-ubuntu/launch-instance-selfservice.html
