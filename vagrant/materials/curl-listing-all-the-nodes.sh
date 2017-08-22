9e3263c89475dbc1f024f5f6f95cc1bb377feb83
Jim Lin <jimlintw922@gmail.com>
2017-08-22 11:50:25 +0800

    use json instead of xml

--- vagrant/materials/curl-listing-all-the-nodes.sh
+++ vagrant/materials/curl-listing-all-the-nodes.sh
@@ -2,6 +2,6 @@
 
 CONTROLLER=192.168.34.101
 
-curl -H "Accept: application/xml" --user admin:admin http://$CONTROLLER:8181/restconf/operational/opendaylight-inventory:nodes/ | xmllint --format -
+curl -H "Accept: application/json" --user admin:admin http://$CONTROLLER:8181/restconf/operational/opendaylight-inventory:nodes/ | jq '.'
 
 # Reference https://wiki.opendaylight.org/view/OpenDaylight_OpenFlow_Plugin::End_to_End_Inventory
--- vagrant/materials/curl-listing-the-specific-node.sh
+++ vagrant/materials/curl-listing-the-specific-node.sh
@@ -3,6 +3,6 @@
 CONTROLLER=192.168.34.101
 NODE_ID=192356055215691
 
-curl -H "Accept: application/xml" --user admin:admin http://$CONTROLLER:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:$NODE_ID | xmllint --format -
+curl -H "Accept: application/json" --user admin:admin http://$CONTROLLER:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:$NODE_ID | jq '.'
 
 # Reference https://wiki.opendaylight.org/view/OpenDaylight_OpenFlow_Plugin::End_to_End_Inventory
