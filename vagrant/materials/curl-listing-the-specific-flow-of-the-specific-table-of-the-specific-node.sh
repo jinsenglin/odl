8ad2db5e731c3b77e34593de7f7d21a078aea06c
Jim Lin <jimlintw922@gmail.com>
2017-08-22 12:13:28 +0800

    update materials

--- vagrant/materials/curl-listing-the-specific-flow-of-the-specific-table-of-the-specific-node.sh
+++ vagrant/materials/curl-listing-the-specific-flow-of-the-specific-table-of-the-specific-node.sh
@@ -6,5 +6,6 @@ TABLE_ID=0
 FLOW_ID=L2switch-0
 
 curl -H "Accept: application/json" --user admin:admin http://$CONTROLLER:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:$NODE_ID | jq ".node[0][\"flow-node-inventory:table\"][] | select(.id == $TABLE_ID) | .flow[] | select(.id == \"$FLOW_ID\")"
+# Against `ovs-ofctl dump-flows`
 
 # Reference https://wiki.opendaylight.org/view/OpenDaylight_OpenFlow_Plugin::End_to_End_Inventory
