ba1af9045ea19f6707e236a944cdd8707420bcb9
Jim Lin <jimlintw922@gmail.com>
2017-08-24 10:11:36 +0800

    update update-odl-controller.sh

--- with-openstack/vagrant/update-odl-controller.sh
+++ with-openstack/vagrant/update-odl-controller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # 1-1
-# feature:install odl-netvirt-openstack odl-dlux-core odl-mdsal-apidocs
+# Note: official reference only mentions `feature:install odl-netvirt-openstack odl-dlux-core odl-mdsal-apidocs`
 # feature:install odl-restconf odl-l2switch-switch odl-mdsal-apidocs odl-dlux-core odl-dlux-all odl-netvirt-openstack
