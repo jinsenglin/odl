6f9550c32fd5a13be0d4abecc1c478b919620629
Jim Lin <jimlintw922@gmail.com>
2017-08-24 11:55:15 +0800

    update update-odl-controller.sh

--- with-openstack/vagrant/update-odl-controller.sh
+++ with-openstack/vagrant/update-odl-controller.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# [ ? ] 0.4.4-Beryllium-SR4
+# [ ? ] 0.5.1-Boron-SR1
+# [ x ] 0.5.3-Boron-SR3
+# [ x ] 0.5.4-Boron-SR4
+
 # 1-1
 # Note: official reference only mentions `feature:install odl-netvirt-openstack odl-dlux-core odl-mdsal-apidocs`
 # feature:install odl-restconf odl-l2switch-switch odl-mdsal-apidocs odl-dlux-core odl-dlux-all odl-netvirt-openstack
