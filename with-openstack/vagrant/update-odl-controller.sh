1ba22056c6cef55e30fd2e611b0dd121a881ad89
Jim Lin <jimlintw922@gmail.com>
2017-08-28 17:49:51 +0800

    update update-odl-controller.sh

--- with-openstack/vagrant/update-odl-controller.sh
+++ with-openstack/vagrant/update-odl-controller.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# [ x ] 0.5.1-Boron-SR1
-# [ x ] 0.5.3-Boron-SR3
-# [ x ] 0.5.4-Boron-SR4
-
 # 1-1
-# Note: official reference only mentions `feature:install odl-netvirt-openstack odl-dlux-core odl-mdsal-apidocs`
-# feature:install odl-restconf odl-l2switch-switch odl-mdsal-apidocs odl-dlux-core odl-dlux-all odl-netvirt-openstack
+# Change Boron-SR3 to Boron-SR4
+# feature:install odl-netvirt-openstack odl-dlux-core odl-mdsal-apidocs
