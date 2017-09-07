0f2f0be40c69a372df957c4f29a170daf132f169
Jim Lin <jimlintw922@gmail.com>
2017-09-07 17:21:17 +0800

    update update-os-compute.sh

--- with-openstack/vagrant/update-os-compute.sh
+++ with-openstack/vagrant/update-os-compute.sh
@@ -26,6 +26,11 @@ apt-get install -y python-networking-odl=1:2.0.1~git20160926.416a5c7-0ubuntu1~cl
 sed -i "/^service_plugins = / d" /etc/neutron/neutron.conf
 sed -i "/^\[DEFAULT\]$/ a service_plugins = odl-router" /etc/neutron/neutron.conf
 
+# 1-15
+ovs-vsctl set Open_vSwitch . other_config:provider_mappings=external:enp0s10
+ovs-vsctl show
+ovs-vsctl get Open_vSwitch . other_config
+
 <<LOG
 `ovs-ofctl -OOpenFlow13 dump-flows br-int | grep 10.0.3.240`
 
